8d6b1147ab596705a49f260be12d79dba3561350
Aleksandar Ilanković <ilankovicaleksandar@gmail.com>
2026-05-28 22:08:42 +0200

    Fix streams not being disposed in FileService

--- Infrastructure.Persistence/FileService/Implementation/FileService.cs
+++ Infrastructure.Persistence/FileService/Implementation/FileService.cs
@@ -1,32 +1,48 @@
 ﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Infrastructure.Persistence.FileService.Interfaces;
 using Infrastructure.Persistence.FileService.Options;
 using Microsoft.Extensions.Options;
+using System.IO;
 
 namespace Infrastructure.Persistence.FileService.Implementation
 {
     public class FileService : IFileService
     {
-        private readonly BlobClient blobClient;
+        private readonly AzureBlobStorageOptions options;
 
-        public FileService(IOptions<AzureBlobStorageOptions> options) 
+        private BlobClient CreateBlobClient(string path) => new(options.ConnectionString, "vrirs", path);
+
+        public FileService(IOptions<AzureBlobStorageOptions> options)
         {
-            blobClient = new(new Uri(options.Value.ConnectionString));
+            this.options = options.Value;
         }
 
-        public Task DeleteAsync(string path, CancellationToken cancellationToken)
+        public async Task DeleteAsync(string path, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var blobClient = CreateBlobClient(path);
+            var azureResponse = await blobClient.DeleteAsync(DeleteSnapshotsOption.IncludeSnapshots, cancellationToken: cancellationToken);
         }
 
-        public Task DownloadAsync(string path, CancellationToken cancellationToken)
+        public async Task<byte[]> DownloadAsync(string path, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var blobClient = CreateBlobClient(path);
+            var blobReadOptions = new BlobOpenReadOptions(allowModifications: false);
+
+            using Stream azureStream = await blobClient.OpenReadAsync(blobReadOptions, cancellationToken);
+            using MemoryStream memoryStream = new();
+
+            await azureStream.CopyToAsync(memoryStream, cancellationToken);
+            return memoryStream.ToArray();
+
         }
 
-        public Task UploadAsync(Stream stream, string path, CancellationToken cancellationToken)
+        public async Task UploadAsync(byte[] bytes, string path, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var blobClient = CreateBlobClient(path);
+
+            using MemoryStream memoryStream = new(bytes);
+            await blobClient.UploadAsync(memoryStream, cancellationToken);
         }
     }
 }
